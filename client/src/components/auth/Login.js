39424c65276ca775a2b5b2b84c3fd6b0510ff139
Ericcentrik <enaiman1@gmail.com>
2020-05-28 19:57:34 -0400

    got login to redirect to dashboard, filled out starter code to dashboard

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+
 class Login extends Component {
   constructor() {
     super();
@@ -9,6 +14,19 @@ class Login extends Component {
       errors: {},
     };
   }
+
+  componentWillReceiveProps(nextProps) {
+// when user logs in they will get redirected to the dashboard (which is a private route)
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); 
+    }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors,
+      });
+    }
+  }
+
   onChange = (e) => {
     this.setState({ [e.target.id]: e.target.value });
   };
@@ -18,7 +36,8 @@ class Login extends Component {
       email: this.state.email,
       password: this.state.password,
     };
-    console.log(userData);
+    // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+    this.props.loginUser(userData);
   };
   render() {
     const { errors } = this.state;
@@ -40,26 +59,40 @@ class Login extends Component {
             </div>
             <form noValidate onSubmit={this.onSubmit}>
               <div className="input-field col s12">
-                  {/* input field for email */}
+                {/* input field for email */}
                 <input
                   onChange={this.onChange}
                   value={this.state.email}
                   error={errors.email}
                   id="email"
                   type="email"
+                  className={classnames("", {
+                    invalid: errors.email || errors.emailnotfound,
+                  })}
                 />
                 <label htmlFor="email">Email</label>
+                <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
               </div>
               <div className="input-field col s12">
-                  {/* input field for password */}
+                {/* input field for password */}
                 <input
                   onChange={this.onChange}
                   value={this.state.password}
                   error={errors.password}
                   id="password"
                   type="password"
+                  className={classnames("", {
+                    invalid: errors.password || errors.passwordincorrect,
+                  })}
                 />
                 <label htmlFor="password">Password</label>
+                <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
@@ -82,4 +115,19 @@ class Login extends Component {
     );
   }
 }
-export default Login;
+
+// using prop type to validate the data being passed is in the right format
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+
+// allows us to get our state from Redux and map it to props
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+
+// we are connecting our component to our Redux store
+export default connect(mapStateToProps, { loginUser })(Login);
