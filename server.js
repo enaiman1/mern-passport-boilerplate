24206b7a414694c9c0df158f38ca8c77b7debc37
Ericcentrik <enaiman1@gmail.com>
2020-05-28 13:43:03 -0400

    added passport and test both post routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users")
 
 const app = express();
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
@@ -15,10 +18,19 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB successfully connected"))
+  .then(() => console.log("MongoDB 🔌 connected 🤘"))
   .catch((err) => console.log(err));
+
+  // Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 
 app.listen(port, () =>
-  console.log(`🚀 Server is listening at http://localhost:${port} 🧭`)
+  console.log(`🚀 Server is 🤙listening at 🧭 http://localhost:${port} `)
 );
