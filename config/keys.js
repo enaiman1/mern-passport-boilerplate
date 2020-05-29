2ed1d3f539c3e2f2543be27463796dcb6fe463de
Ericcentrik <enaiman1@gmail.com>
2020-05-28 21:32:49 -0400

    deploying

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
+require('dotenv/config')
+
+
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/mern-passport",
+    // use this when using local mongo db
+    // mongoURI: "mongodb://localhost:27017/mern-passport",
+
+    // this hooks up to mlab/heroku
+    mongoURI: process.env.MLAB_URI,
     secretOrKey: "ericcentrik"
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path')
+require('dotenv').config
 
 const users = require("./routes/api/users")
 
@@ -13,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = process.env.MONGODB_URI || require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
@@ -29,6 +31,15 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+
+// helps serve static assest if in production (deployed to heroku)
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 app.listen(port, () =>
